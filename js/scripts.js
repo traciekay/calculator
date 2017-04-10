27cc43edaa8e8694d8657b8d0e929c6b13b9f65a
traciekay <tkaonje@gmail.com>
2017-04-10 19:21:21 +0300

    added new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
+//Business logic
 var add = function(number1, number2) {
        return number1 + number2;
      };
 
-     var number1 = parseInt(prompt("Enter a number:"));
-     var number2 = parseInt(prompt("Enter another number:"));
-     var result = add(number1, number2);
-     alert(result);
-
-     var sub = function(number1, number2) {
+var subtract = function(number1, number2) {
             return number1 - number2;
           };
-          var number1 = parseInt(prompt("Enter a number:"));
-          var number2 = parseInt(prompt("Enter another number:"));
-          var result = sub(number1, number2);
-          alert(result);
+
+var multiply = function(number1, number2) {
+                      return number1 * number2;
+                    };
+
+var division = function(number1, number2) {
+                                return number1 / number2;
+                              };
+    // Everything below this line is user interface (or front-end) logic
+
+      $(document).ready(function(){
+        $("form#add").submit(function(event) {
+          event.preventDefault();
+          var number1 = parseInt($("#add1").val());
+          var number2 = parseInt($("#add2").val());
+          var result = add(number1, number2);
+          $("#output").text(result);
+        });
+      });
